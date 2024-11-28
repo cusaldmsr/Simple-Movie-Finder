d19ca90c9cce3fed8635d01b661d928b39d8c55f
Kusal Damsara <kusal2002d@Gmail.com>
2024-11-28 22:12:03 +0530

    create sample app

--- index.js
+++ index.js
@@ -1,54 +1,8 @@
-// console.log("Hellow World");
+function search(){
+let movieName = "The walking dead";
 
-// isPassword = false;
-// if(isPassword){
-//     console.log("Hello World");
-// }else{
-//     console.log("Hello World 1");
-// }
+let httpRequest = new XMLHttpRequest();
 
+// let url= "https://www.omdbapi.com/?s=" + movieName + "&apikey=f954ef38";
 
-// for(let i=0; i<10; i++){
-//     console.log(i);
-// }
-
-let students = [
-    {
-        name: "John",
-        age: 20,
-        gender: "male",
-        address: {
-            street: "123 Main St",
-            city: "New York",
-            state: "NY",
-            zip: "10001"
-        },
-        parent: {
-            name: "Jane",
-            age: 45,
-            occupation: "Teacher"
-        }
-    },
-    {
-        name: "Janne",
-        age: 25,
-        gender: "female",
-        address: {
-            street: "456 Elm St",
-            city: "Los Angeles",
-            state: "CA",
-            zip: "90001"
-        },
-        parent: {
-            name: "John",
-            age: 50,
-            occupation: "Doctor"
-        }
-    }
-];
-
-
-console.log(student.name);
-
-student.name= "Bob";
-console.log(student.name);
+}
