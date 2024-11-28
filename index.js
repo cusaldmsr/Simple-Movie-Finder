0e5cb055368a1b5d3ba9c703e49572e43e0174d8
Kusal Damsara <kusal2002d@Gmail.com>
2024-11-28 22:29:46 +0530

    update Js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-function search(){
-let movieName = "The walking dead";
+function search() {
+    let inputPanel = document.getElementById("input");
+    let movieName = inputPanel.value;
 
-let httpRequest = new XMLHttpRequest();
+    let htmlRequest = new XMLHttpRequest();
 
-// let url= "https://www.omdbapi.com/?s=" + movieName + "&apikey=f954ef38";
+    let url = "http://www.omdbapi.com/?apikey=f954ef38&t=" + movieName;
 
+    htmlRequest.open("GET", url);
+    htmlRequest.responseType = "json";
+
+    htmlRequest.send();
+
+    htmlRequest.onload = function() {
+        console.log(htmlRequest.response);
+        let poster = document.getElementById("poster");
+        poster.src = htmlRequest.response.Poster;
+
+        let title = document.getElementById("title");
+        title.textContent = htmlRequest.response.Title;
+
+        
+    }
 }
