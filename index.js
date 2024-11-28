0d5615f88c1efbdb29bd541a57b0a98291722c67
Kusal Damsara <kusal2002d@Gmail.com>
2024-11-28 21:58:24 +0530

    add another student

--- index.js
+++ index.js
@@ -12,23 +12,40 @@
 //     console.log(i);
 // }
 
-let student =
-{
-    name:"John",
-    age:20,
-    gender:"male",
-    address:{
-        street:"123 Main St",
-        city:"New York",
-        state:"NY",
-        zip:"10001"
+let students = [
+    {
+        name: "John",
+        age: 20,
+        gender: "male",
+        address: {
+            street: "123 Main St",
+            city: "New York",
+            state: "NY",
+            zip: "10001"
         },
-    parent:{
-        name:"Jane",
-        age:45,
-        occupation:"Teacher"
+        parent: {
+            name: "Jane",
+            age: 45,
+            occupation: "Teacher"
+        }
+    },
+    {
+        name: "Janne",
+        age: 25,
+        gender: "female",
+        address: {
+            street: "456 Elm St",
+            city: "Los Angeles",
+            state: "CA",
+            zip: "90001"
+        },
+        parent: {
+            name: "John",
+            age: 50,
+            occupation: "Doctor"
         }
     }
+];
 
 
 console.log(student.name);
