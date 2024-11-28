87790594eceae41cb64332fdcc1799a0db234a56
Kusal Damsara <kusal2002d@Gmail.com>
2024-11-28 21:42:05 +0530

    create forloop

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // console.log("Hellow World");
 
-isPassword = false;
-if(isPassword){
-    console.log("Hello World");
-}else{
-    console.log("Hello World 1");
+// isPassword = false;
+// if(isPassword){
+//     console.log("Hello World");
+// }else{
+//     console.log("Hello World 1");
+// }
+
+
+for(let i=0; i<10; i++){
+    console.log(i);
 }
